c554551344fecfb8705d258a96a8775b76ee5096
Kornilov <dm.korniloff@gmail.com>
2022-04-17 15:47:09 +0300

    17.04.2022

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, {Express} from 'express';
+import { Server } from 'http';
+import { inject, injectable } from 'inversify';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import { UserController } from './users/users.controller';
+import 'reflect-metadata';
+
+@injectable()
+export class App{
+	app: Express;
+	server: Server;
+	port: number;
+	
+
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
+) {
+		this.app = express();
+		this.port = 8000;
+		
+	}
+
+	useRoutes() {
+		this.app.use('/users', this.userController.router)
+	}
+
+	useExeptionFilter() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
+
+	public async init() {
+		this.useRoutes();
+		this.useExeptionFilter();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
+		//console.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
+}
---
+++ src/users/users.controller.interface.ts
@@ -0,0 +1,9 @@
+import { Request, Response,  NextFunction } from 'express';
+
+export interface IUserController {
+
+	login :(req: Request, res: Response, next: NextFunction) => void;
+
+	register :(req: Request, res: Response, next: NextFunction) => void;
+	
+}
